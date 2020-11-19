4d193b90c8f11009b6695d8d2dac33012adbd0ce
Lucas Bortolazzo <lucas.dolberth@yahoo.com>
2020-11-18 21:12:41 -0300

    added serializer

--- customers-backend/core/models.py
+++ customers-backend/core/models.py
@@ -3,5 +3,7 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
-    city = models.CharField(100)
+    city = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
--- customers-backend/core/views.py
+++ customers-backend/core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from core.models import Customer
+from core.serializers import CustomerSerializer
 
-# Create your views here.
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all().order_by('-id')
+    serializer_class = CustomerSerializer
+
